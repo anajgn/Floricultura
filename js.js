04f911f8df69e43efe6c1362352b9757cd8ebe8b
anajgn <162343500+anajgn@users.noreply.github.com>
2024-05-02 12:12:46 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,33 +1,33 @@
-deixar URL="dados.xml";
-Var id = 0;
+let url="dados.xml";
+var id = 0;
 
-$.Ajax(URL)
-    .terminado(função(XML){
-        $(XML).encontrar("flor").cada(função(){
-            $("#cards").acrescentar('<div class="card"><a href="individual.html?id='+$(este).encontrar("Id").Texto()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(este).encontrar("Id").Texto() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(este).encontrar("nome").Texto() +'</p> <p>'+ $(este).encontrar("Alcunha").Texto()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(este).encontrar("recompensa").Texto()+' </p><p> Status:'+ $(este).encontrar("Status").Texto()+'</a> </p><\div>');
+$.ajax(url)
+    .done(function(xml){
+        $(xml).find("pirata").each(function(){
+            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
         });
     })
-    .falhar(função(){
-        alerta("Ocorreu um erro na leitura do arquivo XML.");
+    .fail(function(){
+        alert("Ocorreu um erro na leitura do arquivo XML.");
     })
 
    
 
 
-    Var url_string = janela.localização.HREF;
-    Var URL1 = Novo URL(url_string);
-    Var id = parseInt(URL1.pesquisaParams.Obter("Id"));
+    var url_string = window.location.href;
+    var url1 = new URL(url_string);
+    var id = parseInt(url1.searchParams.get("id"));
 
-    $.Ajax(URL)
-    .terminado(função(XML){
-            $(XML).encontrar("flor").cada(função(){
-                Var pdv = parseInt($(este).encontrar("Id").Texto())
-                se(id == pdv){
-                $("#individual").acrescentar('<div class="card"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(este).encontrar("Id").Texto() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(este).encontrar("nome").Texto() +'</p> <p>'+ $(este).encontrar("Alcunha").Texto()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(este).encontrar("recompensa").Texto()+' </p><p> Status:'+ $(este).encontrar("Status").Texto()+' </p><\div>');
+    $.ajax(url)
+    .done(function(xml){
+            $(xml).find("pirata").each(function(){
+                var pos = parseInt($(this).find("id").text())
+                if(id == pos){
+                $("#individual").append('<div class="card"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
                 }
             });
         
     })
-    .falhar(função(){
-        alerta("Ocorreu um erro na leitura do arquivo XML.");
+    .fail(function(){
+        alert("Ocorreu um erro na leitura do arquivo XML.");
     })
